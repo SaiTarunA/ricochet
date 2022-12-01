144fff4526e07764274afc012f9f837bd2adbafb
purnareddys <purnareddy.2013@gmail.com>
2022-12-01 13:01:27 +0530

    purna: added listing component files

---
+++ src/components/listing/GlobalStyles.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+export const lightGray = "#f2f2f2";
+export const GlobalStyles = createGlobalStyle`
+ * {
+  font-family: 'Montserrat', sans-serif;
+  margin: 0;
+}
+ body { 
+  background: #1C2333; 
+  /* background-image: url("https://images.pexels.com/photos/370799/pexels-photo-370799.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"); */
+ }
+`;
---
+++ src/components/listing/mockAPI.js
@@ -0,0 +1,25 @@
+const API = [
+  {
+    name: "Apple",
+    price: 120,
+    quantity: 1,
+    inCart: false,
+    src:
+      "https://images.pexels.com/photos/102104/pexels-photo-102104.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+  },
+  {
+    name: "Maggi",
+    price: 45,
+    quantity: 1,
+    inCart: false,
+    src: "https://m.media-amazon.com/images/I/71jfrTizZTL._SX522_.jpg"
+  },
+  {
+    name: "Coke",
+    price: 120,
+    quantity: 1,
+    inCart: false,
+    src: "https://images.pexels.com/photos/39720/pexels-photo-39720.jpeg"
+  }
+];
+export default API;
